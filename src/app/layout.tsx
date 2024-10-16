5befd06c3638fecf261e7956e625d66da12be44e
Keith Glines <keithglines@yahoo.com>
2024-10-16 16:17:01 -0400

    Query Provider in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { QueryProvider } from "@/components/query-provider";
 import { cn } from "@/lib/utils";
 import "./globals.css";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, "antialiased min-h-screen")}>
-        {children}
+        <QueryProvider>
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
